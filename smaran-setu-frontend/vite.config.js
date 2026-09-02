1ed40e4d1ff31ed325c6997d0803d09623dc83a0
akashgitty <akashshukla7612@gmail.com>
2026-09-02 20:03:25 +0530

    new change

--- smaran-setu-frontend/vite.config.js
+++ smaran-setu-frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/smaran-setu-sih26003/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
